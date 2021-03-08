f06f753cadac3760de50ead87c265b19b69455e5
Diana Liao <dijali@gmail.com>
2021-03-08 09:44:28 -0500

    commit tree 4b628ec4d60740b55dd1734e94f2b512712d56c6

--- solution.rb
+++ solution.rb
@@ -1,7 +1,55 @@
+require 'pry'
+
 class Deck
+  attr_reader :cards
+
+  def initialize
+    Card.all.clear
+
+    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    suits = ["Hearts", "Clubs", "Spades", "Diamonds"]
+
+    ranks.each do |rank_name|
+      suits.each do |suit_name|
+        Card.new(suit_name, rank_name)
+      end
+    end
+
+    @cards = Card.all
+
+  end
+
+  def choose_card
+    # @cards.shuffle.pop
+    chosen_card = @cards.sample
+    @cards.delete(chosen_card)
+    #chosen_card
+
+  end
+
 
 end
 
 class Card
+
+  attr_accessor :suit, :rank
+
+  @@all_cards = []
+
+  def initialize (suit, rank)
+    @suit = suit
+    @rank = rank
+
+    @@all_cards << self
+  end
+
+  def self.all
+    @@all_cards
+  end
+
+
+
+
+
 
 end
